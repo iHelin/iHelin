2a265fa392ba2b71ce790a619806bd4722e0cd21
linhe2 <linhe2@iflytek.com>
2018-05-30 23:20:45 +0800

    修改文章显示

--- frontend/src/pages/article.vue
+++ frontend/src/pages/article.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
-        <div v-if="article" v-html="article.content"></div>
+        <div v-if="article">
+            <h1 style="text-align: center">{{article.title}}</h1>
+            <p style="text-align: center">
+                <small>作者：{{article.author}}</small>
+                <small>阅读数：{{article.readNum}}</small>
+                <small>更新时间：{{article.updateTime}}</small>
+            </p>
+            <p><code>{{article.summary}}</code></p>
+            <div v-html="article.content"></div>
+        </div>
         <div v-else>文章不存在</div>
     </div>
 </template>
