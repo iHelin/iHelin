2a581397ee5d60caeac9d5ece3824643a448e453
linhe2 <linhe2@iflytek.com>
2018-06-16 00:44:28 +0800

    去除多余菜单

--- frontend/src/pages/home.vue
+++ frontend/src/pages/home.vue
@@ -6,14 +6,6 @@
                 <el-menu-item index="/articles">文章</el-menu-item>
                 <el-menu-item index="/music">音乐</el-menu-item>
                 <el-menu-item index="/feedback">反馈</el-menu-item>
-                <el-submenu index="2">
-                    <template slot="title">我的工作台</template>
-                    <el-menu-item index="/test">测试</el-menu-item>
-                    <el-submenu index="2-4">
-                        <template slot="title">工作台4</template>
-                        <el-menu-item index="/test">选项1</el-menu-item>
-                    </el-submenu>
-                </el-submenu>
             </el-menu>
         </el-header>
         <el-main>
