1672c56ec6b019368aabce4ed1e3d9c716a23ba7
linhe2 <linhe2@iflytek.com>
2018-06-16 00:09:32 +0800

    修改接口

--- src/main/java/me/ianhe/isite/controller/TestController.java
+++ src/main/java/me/ianhe/isite/controller/TestController.java
@@ -1,7 +1,6 @@
 package me.ianhe.isite.controller;
 
 import com.google.common.collect.Lists;
-import me.ianhe.isite.utils.Constant;
 import me.ianhe.isite.utils.JsonUtil;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
@@ -12,8 +11,10 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -36,12 +37,6 @@ public class TestController extends BaseController {
     public Map<String, Object> sendMessage(@RequestBody Map<String, String> data) {
         webSocket.sendMessage(data.get("data"));
         return success();
-    }
-
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    @GetMapping(Constant.LOGIN_PAGE)
-    public Map<String, Object> login() {
-        return success("请登录!");
     }
 
     @GetMapping("bus")
