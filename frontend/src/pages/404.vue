bb227e9ef5504c87c72f074530633ab208a435d1
linhe2 <linhe2@iflytek.com>
2018-07-15 16:36:05 +0800

    添加公益404

--- frontend/src/pages/404.vue
+++ frontend/src/pages/404.vue
@@ -1,6 +1,6 @@
 <template>
     <div style="text-align: center;margin-top: 60px;">
-        <iframe scrolling='no' frameborder='0' src='http://yibo.iyiyun.com/Home/Distribute/ad404/key/1341657'
+        <iframe scrolling='no' frameborder='0' src='https://yibo.iyiyun.com/Home/Distribute/ad404/key/1341657'
                 width='654' height='470' style='display:block;margin: 0 auto;'></iframe>
     </div>
 </template>
