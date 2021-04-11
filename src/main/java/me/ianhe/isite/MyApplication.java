07ebdbb2fe2e2eb80c9005421b82a75e8b142b62
iHelin <ihelin@outlook.com>
2021-04-11 19:21:02 +0800

    update

--- src/main/java/me/ianhe/isite/MyApplication.java
+++ src/main/java/me/ianhe/isite/MyApplication.java
@@ -2,6 +2,7 @@ package me.ianhe.isite;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -34,6 +35,7 @@ public class MyApplication {
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return interceptor;
     }
 
