8f14f2ab16a89a9a8c9162915560036c5cfd64ec
iHelin <ihelin@outlook.com>
2021-04-19 09:03:47 +0800

    1

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,32 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from '@/store'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import '@/icons'
-import '@/assets/scss/index.scss'
-import httpRequest from '@/utils/httpRequest' // api: https://github.com/axios/axios
-import {formatPayType, formatStatus, isAuth} from '@/utils';
-import cloneDeep from 'lodash/cloneDeep';
-
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "@/store";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import "@/icons";
+import "@/assets/scss/index.scss";
+import httpRequest from "@/utils/httpRequest"; // api: https://github.com/axios/axios
+import { formatPayType, formatStatus, isAuth } from "@/utils";
+import cloneDeep from "lodash/cloneDeep";
 
 Vue.use(ElementUI);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 // 挂载全局
 Vue.prototype.$http = httpRequest; // ajax请求方法
-Vue.prototype.isAuth = isAuth;     // 权限方法
-Vue.filter('formatStatus', formatStatus);
-Vue.filter('formatPayType', formatPayType);
+Vue.prototype.isAuth = isAuth; // 权限方法
+Vue.filter("formatStatus", formatStatus);
+Vue.filter("formatPayType", formatPayType);
 
 // 保存整站vuex本地储存初始状态
 window.SITE_CONFIG = {};
-window.SITE_CONFIG['storeState'] = cloneDeep(store.state)
+window.SITE_CONFIG["storeState"] = cloneDeep(store.state);
 
 new Vue({
     router,
     store,
     render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
