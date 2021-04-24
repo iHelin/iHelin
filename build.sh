b2efcc0611840dca6080c8b433f79b68d48be31e
iHelin <ihelin@outlook.com>
2021-04-24 20:10:08 +0800

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 mvn clean && mvn package
 mvn docker:build
-docker push ihelin/ihelin:2.0.0
+#docker push ihelin/ihelin:2.0.0
+docker tag ihelin/ihelin:2.0.0 registry.cn-qingdao.aliyuncs.com/ihelin/ihelin:2.0.0
+docker push registry.cn-qingdao.aliyuncs.com/ihelin/ihelin:2.0.0
 # 删除none镜像
 docker rmi $(docker images | grep "none" | awk '{print $3}')
